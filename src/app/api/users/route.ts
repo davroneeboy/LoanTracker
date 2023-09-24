ca6aedb2f93fc818f530849005d434c281cbcd4d
davroneeboy <davron.tukhtamuratov.1808@gmail.com>
2023-09-24 01:54:29 -0400

    page to post new users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export const GET = async () => {
   const res = await fetch(`${process.env.GL_API}/users`, {
     headers: {
       "Content-Type": "application/json",
@@ -9,4 +9,19 @@ export async function GET() {
   const data = await res.json();
 
   return NextResponse.json(data);
-}
+};
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const res = await fetch(`${process.env.GL_API}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const data = await res.json();
+
+  return NextResponse.json(data);
+};
