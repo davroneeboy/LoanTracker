793c2d6e67011e222396a3cc3e734024b84af715
davroneeboy <davron.tukhtamuratov.1808@gmail.com>
2023-09-24 03:27:41 -0400

    get loans from userId

---
+++ src/app/api/users/[id]/loans/route.ts
@@ -0,0 +1,22 @@
+import LoanSchema from "@/types/loan.type";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  const { pathname } = req.nextUrl;
+  const [, api, users, userId, loans] = pathname.split("/");
+
+  if (!(api === "api" && users === "users" && loans === "loans")) {
+    console.error("Invalid pathname:", req.nextUrl);
+    return NextResponse.json([]);
+  }
+
+  const url = `${process.env.GL_API}/users/${userId}/loans`;
+  const res = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: LoanSchema[] = await res.json();
+
+  return NextResponse.json(data);
+};
