fd5aace7ff308b26b74397a76a270287d377288f
davroneeboy <davron.tukhtamuratov.1808@gmail.com>
2023-09-25 13:47:29 -0400

    error handling on loan update page

--- src/app/loans/[loanId]/update/page.tsx
+++ src/app/loans/[loanId]/update/page.tsx
@@ -15,6 +15,11 @@ const UpdateLoan = ({ params }: { params: { loanId: string } }) => {
   const { user, setUser } = useUserContext();
 
   const currentLoan = loans[parseInt(loanId)];
+
+  if (!currentLoan) {
+    router.push(`/users/${user}`);
+  }
+
   const { amount, apr, term, status, owner_id: ownerId } = currentLoan;
 
   const onFinish = async (values: LoanSchemaBase) => {
@@ -41,7 +46,11 @@ const UpdateLoan = ({ params }: { params: { loanId: string } }) => {
         setLoans(updatedLoans);
 
         const data: LoanSchema = await response.json();
-        router.push(`/loans/${data.id}`);
+        if (data?.id) {
+          router.push(`/loans/${data.id}`);
+        } else {
+          router.push(`/users/${user}`);
+        }
       } else {
         console.error("Error:", response.status);
       }
@@ -55,74 +64,83 @@ const UpdateLoan = ({ params }: { params: { loanId: string } }) => {
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Amount"
-        name="amount"
-        rules={[{ required: true, message: "Please input an amount!" }]}
-        initialValue={amount}
+    <>
+      <Title>{`Update Loan ${loanId}`}</Title>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input type="number" />
-      </Form.Item>
+        <Form.Item
+          label="Amount"
+          name="amount"
+          rules={[{ required: true, message: "Please input an amount!" }]}
+          initialValue={amount}
+        >
+          <Input type="number" />
+        </Form.Item>
 
-      <Form.Item
-        label="APR"
-        name="apr"
-        rules={[{ required: true, message: "Please input the APR!" }]}
-        initialValue={apr}
-      >
-        <Input type="number" />
-      </Form.Item>
+        <Form.Item
+          label="APR"
+          name="apr"
+          rules={[{ required: true, message: "Please input the APR!" }]}
+          initialValue={apr}
+        >
+          <Input type="number" />
+        </Form.Item>
 
-      <Form.Item
-        label="Term"
-        name="term"
-        rules={[{ required: true, message: "Please input the term!" }]}
-        initialValue={term}
-      >
-        <Input type="number" />
-      </Form.Item>
+        <Form.Item
+          label="Term"
+          name="term"
+          rules={[{ required: true, message: "Please input the term!" }]}
+          initialValue={term}
+        >
+          <Input type="number" />
+        </Form.Item>
 
-      <Form.Item
-        label="Status"
-        name="status"
-        rules={[{ required: true, message: "Please select a status!" }]}
-        initialValue={status}
-      >
-        <Select
-          options={[
-            { value: "active", label: "Active" },
-            { value: "approved", label: "Approved" },
-            { value: "inactive", label: "Inactive" },
-            { value: "forbearance", label: "Forbearance" },
-          ]}
-        />
-      </Form.Item>
+        <Form.Item
+          label="Status"
+          name="status"
+          rules={[{ required: true, message: "Please select a status!" }]}
+          initialValue={status}
+        >
+          <Select
+            options={[
+              { value: "active", label: "Active" },
+              { value: "approved", label: "Approved" },
+              { value: "inactive", label: "Inactive" },
+              { value: "forbearance", label: "Forbearance" },
+            ]}
+          />
+        </Form.Item>
 
-      <Form.Item
-        label="Owner ID"
-        name="owner_id"
-        rules={[{ required: true, message: "Please input an owner ID!" }]}
-        initialValue={ownerId}
-      >
-        <Input type="number" disabled={true} />
-      </Form.Item>
+        <Form.Item
+          label="Owner ID"
+          name="owner_id"
+          rules={[{ required: true, message: "Please input an owner ID!" }]}
+          initialValue={ownerId}
+        >
+          <Input type="number" disabled={true} />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          {user === ownerId ? (
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          ) : (
+            <Button type="primary" htmlType="submit" disabled={true}>
+              Only Owners Can Change Loan
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
